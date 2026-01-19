7e08bf377611c192b7945ca118bf80c0726bda3e
d41dev <d41dev@proton.me>
2026-01-19 13:30:01 +0000

    update: add dramatiq for task queuing

--- api/app.py
+++ api/app.py
@@ -1,29 +1,27 @@
+import os
+
 from fastapi import FastAPI, HTTPException, UploadFile, File, Query, Response
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.concurrency import run_in_threadpool
 
 from typing import Optional
-from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 from revelium.errors import ReveliumError, ErrorCode
 from revelium.schemas.api import  GetPromptsRequest, GetPromptsResponse, GetCountResponse, GetLabelsResponse, GetClustersResponse, GetPromptsOverviewResponse, UpdateLabelResponse, GetClustersAccuracyResponse, QueryPromptsRequest, UpdatePromptClusterIdResponse
 from revelium.constants.api import Routes
 from revelium.prompts.cluster import cluster_prompts, get_cluster_plot
-from tempfile import NamedTemporaryFile
-from api.tasks import index_prompts
-
-
 from revelium.schemas.llm import LLMClientConfig
 from revelium.prompts.prompts_manager import PromptsManager
-from revelium.constants.models import OPENAI_API_KEY, DEFAULT_SYSTEM_PROMPT, DEFAULT_OPENAI_MODEL
-from revelium.constants import DEFAULT_CHROMADB_PATH
-from revelium.models.manage import ModelManager
 from revelium.embeddings.helpers import get_embedding_store
-from revelium.prompts.indexer import PromptIndexer
-from revelium.prompts.indexer_listener import PromptIndexListener
 from revelium.providers.llm.openai import OpenAIClient
-from revelium.main import text_embedder
+from revelium.models.manage import ModelManager
+
+from api.tasks import index_prompts
+
+from revelium.constants.models import OPENAI_API_KEY, DEFAULT_SYSTEM_PROMPT, DEFAULT_OPENAI_MODEL
+from revelium.constants import DEFAULT_CHROMADB_PATH, UPLOAD_DIR
 
 MAX_UPLOAD_SIZE = 50 * 1024 * 1024  # 50MB
 
@@ -65,10 +63,14 @@ app.add_middleware(
 #     return JSONResponse(result_dict)
 
 
-UPLOAD_DIR = Path("/data/uploads")
-UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-def get_p_manaer():
+model_manager = ModelManager()
+text_embedder = model_manager.get_text_embedder('all-minilm-l6-v2')
+text_embedder.init()
+
+# TODO: pass client id to get unique collections per client
+def get_prompt_manager():
     prompt_embedding_store = get_embedding_store(DEFAULT_CHROMADB_PATH, PromptsManager.PROMPT_TYPE, 'all-minilm-l6-v2', text_embedder.embedding_dim) 
     cluster_embedding_store = get_embedding_store(DEFAULT_CHROMADB_PATH, PromptsManager.CLUSTER_TYPE, 'all-minilm-l6-v2', text_embedder.embedding_dim) 
     llm = OpenAIClient(OPENAI_API_KEY, LLMClientConfig(model_name=DEFAULT_OPENAI_MODEL, system_prompt=DEFAULT_SYSTEM_PROMPT))
@@ -86,33 +88,28 @@ async def add_prompts_file(file: UploadFile = File(...)):
 async def get_prompts(req: GetPromptsRequest):
     if req.prompt_ids and req.limit:
         raise HTTPException(status_code=400, detail="Prompt ids and limit filtering must be used seperately")
-    prompt_embedding_store = get_embedding_store(DEFAULT_CHROMADB_PATH, PromptsManager.PROMPT_TYPE, 'all-minilm-l6-v2', text_embedder.embedding_dim) 
-    cluster_embedding_store = get_embedding_store(DEFAULT_CHROMADB_PATH, PromptsManager.CLUSTER_TYPE, 'all-minilm-l6-v2', text_embedder.embedding_dim) 
-    llm = OpenAIClient(OPENAI_API_KEY, LLMClientConfig(model_name=DEFAULT_OPENAI_MODEL, system_prompt=DEFAULT_SYSTEM_PROMPT))
-    prompts_manager = PromptsManager(llm_client=llm, prompt_embedding_store=prompt_embedding_store, cluster_embedding_store=cluster_embedding_store)
-
-
+    prompts_manager = get_prompt_manager()
     prompts = await run_in_threadpool(prompts_manager.get_prompts_paginate, req.prompt_ids, req.cluster_id, req.limit, req.offset)
     return JSONResponse(GetPromptsResponse(prompts=prompts).model_dump())
 
 
 @app.post(Routes.QUERY_PROMPTS_ENDPOINT)
 async def query_prompts(req: QueryPromptsRequest):
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     prompts = await run_in_threadpool(prompts_manager.query_prompts, text_embedder, req.query, req.cluster_id, req.limit)
     return JSONResponse(GetPromptsResponse(prompts=prompts).model_dump())
 
 
 @app.get(Routes.COUNT_PROMPTS_ENDPOINT)
 async def count_prompts():
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     count = await run_in_threadpool( prompts_manager.prompt_embedding_store.count)
     return JSONResponse(GetCountResponse(count=count).model_dump())
 
 
 @app.get(Routes.GET_PROMPTS_OVERVIEW_ENDPOINT)
 async def get_prompts_overview():
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     overview = await run_in_threadpool( prompts_manager.get_prompts_overview)
     return JSONResponse(GetPromptsOverviewResponse(**overview.model_dump()).model_dump())
 
@@ -120,7 +117,7 @@ async def get_prompts_overview():
 @app.patch(Routes.BASE_PROMPTS_ENDPOINT)
 async def update_prompt_cluster(prompt_id: str, cluster_id: str):
     try:
-        prompts_manager = get_p_manaer()
+        prompts_manager = get_prompt_manager()
         await run_in_threadpool( prompts_manager.update_prompts , {prompt_id : cluster_id}, {})
     except ReveliumError as e:
         if e.code == ErrorCode.PROMPT_NOT_FOUND:
@@ -134,7 +131,7 @@ async def update_prompt_cluster(prompt_id: str, cluster_id: str):
 
 @app.post(Routes.START_CLUSTERING_ENDPOINT)
 async def start_clustering_prompts():
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     _ = await run_in_threadpool(cluster_prompts, prompts_manager)
     return JSONResponse({"status": "in_progress"}) # testing only
 
@@ -142,14 +139,14 @@ async def start_clustering_prompts():
 async def get_clusters(cluster_id: Optional[str] = Query(None), limit: Optional[str] = Query(None), offset: Optional[str] = Query(None)):
     limit_int = int(limit) if limit not in (None, "") else None
     offset_int = int(offset) if offset not in (None, "") else None
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     clusters = await run_in_threadpool(prompts_manager.get_clusters, cluster_id, limit_int, offset_int, ['metadatas'])
     return JSONResponse(GetClustersResponse(clusters=list(clusters.values())).model_dump())
 
 
 @app.patch(Routes.BASE_CLUSTER_ENDPOINT)
 async def update_cluster_label(cluster_id: str, label: str):
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     updated = await run_in_threadpool(
         prompts_manager.update_cluster_label, cluster_id, label
     )
@@ -161,26 +158,26 @@ async def update_cluster_label(cluster_id: str, label: str):
 
 @app.get(Routes.COUNT_CLUSTERS_ENDPOINT)
 async def count_clusters():
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     count = await run_in_threadpool( prompts_manager.cluster_embedding_store.count)
     return JSONResponse(GetCountResponse(count=count).model_dump())
 
 @app.get(Routes.GET_CLUSTER_LABELS_ENDPOINT)
 async def get_existing_labels():
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     labels = await run_in_threadpool(prompts_manager.get_existing_labels)
     return JSONResponse(GetLabelsResponse(labels=labels).model_dump())
 
 
 @app.get(Routes.GET_CLUSTER_ACCURACY_ENDPOINT)
 async def get_cluster_accuracy():
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     accuracy = await run_in_threadpool(prompts_manager.calculate_cluster_accuracy)
     return JSONResponse(GetClustersAccuracyResponse(accuracy=accuracy).model_dump())
 
 
 @app.get(Routes.GET_CLUSTER_PLOT_ENDPOINT)
 async def get_clusters_plot():
-    prompts_manager = get_p_manaer()
+    prompts_manager = get_prompt_manager()
     img_bytes = await run_in_threadpool(get_cluster_plot, prompts_manager)
     return Response(status_code=200 if img_bytes else 204, content=img_bytes, media_type="image/png")
--- api/tasks.py
+++ api/tasks.py
@@ -1,33 +1,48 @@
-# celery_tasks.py
-from dotenv import load_dotenv
-from revelium.prompts.types import Prompt
-from revelium.main import indexer
 import os
 import json
+import dramatiq
+
+from dramatiq.middleware import AsyncIO
+from dramatiq.brokers.redis import RedisBroker
+from revelium.prompts.types import Prompt
+from revelium.embeddings.helpers import get_embedding_store
+from revelium.prompts.indexer import PromptIndexer
+from revelium.prompts.indexer_listener import PromptIndexListener
+from revelium.providers.llm.openai import OpenAIClient
+from revelium.models.manage import ModelManager
+
+from revelium.schemas.llm import LLMClientConfig
+from revelium.prompts.prompts_manager import PromptsManager
+from revelium.constants.models import OPENAI_API_KEY, DEFAULT_SYSTEM_PROMPT, DEFAULT_OPENAI_MODEL
+from revelium.constants import DEFAULT_CHROMADB_PATH
+from dotenv import load_dotenv
 
 load_dotenv()
 
 REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
-
-import dramatiq
-from dramatiq.middleware import AsyncIO
-from dramatiq.brokers.redis import RedisBroker
-from revelium.main import indexer
-from revelium.prompts.types import Prompt
-import json
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 
 
 redis_broker = RedisBroker(
     host=REDIS_HOST,
-    port=6379,
+    port=REDIS_PORT,
     password=REDIS_PASSWORD,
 )
-redis_broker.add_middleware(AsyncIO())  # <--- critical for async tasks
+redis_broker.add_middleware(AsyncIO())
 dramatiq.set_broker(redis_broker)
+
+model_manager = ModelManager()
+text_embedder = model_manager.get_text_embedder('all-minilm-l6-v2')
+text_embedder.init()
 
 @dramatiq.actor
 async def index_prompts(file_path: str):
     with open(file_path) as f:
         prompts = [Prompt(**p) for p in json.load(f)]
+    prompt_embedding_store = get_embedding_store(DEFAULT_CHROMADB_PATH, PromptsManager.PROMPT_TYPE, 'all-minilm-l6-v2', text_embedder.embedding_dim) 
+    cluster_embedding_store = get_embedding_store(DEFAULT_CHROMADB_PATH, PromptsManager.CLUSTER_TYPE, 'all-minilm-l6-v2', text_embedder.embedding_dim) 
+    llm = OpenAIClient(OPENAI_API_KEY, LLMClientConfig(model_name=DEFAULT_OPENAI_MODEL, system_prompt=DEFAULT_SYSTEM_PROMPT))
+    prompts_manager = PromptsManager(llm_client=llm, prompt_embedding_store=prompt_embedding_store, cluster_embedding_store=cluster_embedding_store)
+    indexer = PromptIndexer(text_embedder, prompt_embedding_store, listener=PromptIndexListener(prompts_manager))
     return await indexer.run(prompts)
