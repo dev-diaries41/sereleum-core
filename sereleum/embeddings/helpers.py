611e37247e4dbdbf86dd12c07f3357a761d74d82
d41dev <d41dev@proton.me>
2026-01-25 01:07:22 +0000

    update: create seperate managers from clusters and prompts

--- sereleum/embeddings/helpers.py
+++ sereleum/embeddings/helpers.py
@@ -1,14 +1,15 @@
 
 from sereleum.providers.types import TextEmbeddingModel
 from sereleum.embeddings.chroma_store import ChromaDBEmbeddingStore
+from sereleum.types import EmbeddingStoreType
 import chromadb
 
 # helps ensure each collection get embeddings of the right size
 def get_embedding_collection_name( type: str, model: TextEmbeddingModel, embed_dim: int) -> str:
     return f"{type}_{model}_{embed_dim}_collection"
     
-def get_embedding_store(prefix:str, model: TextEmbeddingModel, embedding_dim: int ):
+def get_embedding_store(type:EmbeddingStoreType, model: TextEmbeddingModel, embedding_dim: int ):
     client = chromadb.HttpClient(host='chromadb', port=8000, settings=chromadb.Settings(anonymized_telemetry=False))
     return ChromaDBEmbeddingStore(client.get_or_create_collection(
-                get_embedding_collection_name(prefix, model, embedding_dim))
+                get_embedding_collection_name(type, model, embedding_dim))
             ) 
--- sereleum/types.py
+++ sereleum/types.py
@@ -30,3 +30,5 @@ Status = Literal[
     "delayed",
     "queued",
 ]
+
+EmbeddingStoreType = Literal['cluster', 'prompt']
