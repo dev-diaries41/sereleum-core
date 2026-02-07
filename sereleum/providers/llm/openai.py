b1e1ed99ec222fe29040bcee2dba78c06b024d72
d41dev <d41dev@proton.me>
2026-02-07 23:15:25 +0000

    update: implement generate_text_from_images for openai

--- sereleum/providers/llm/openai.py
+++ sereleum/providers/llm/openai.py
@@ -1,8 +1,9 @@
 from typing import Optional, List, Type
 from sereleum.providers.llm.llm_client import LLMClient
 from openai import OpenAI
-from sereleum.schemas.llm import LLMClientConfig, Message
+from sereleum.schemas.llm import LLMClientConfig, Message, ImageMessage, ImageContent
 from sereleum.providers.types import JsonOutput
+
 
 
 class OpenAIClient(LLMClient):
@@ -21,13 +22,19 @@ class OpenAIClient(LLMClient):
         )
         return response.output_text
     
-    def generate_json(self, prompt:str, format: Type[JsonOutput], history: Optional[List[Message]] = None):
+    def generate_json(self, prompt:str, format: Type[JsonOutput], images = None, history = None):
         response = self.openai.responses.parse(
             model=self.config.model_name,
             input=[
                     Message(role="system", content=self.config.system_prompt),
                     *(history or []),
-                    Message(role="user", content=prompt),
+                    ImageMessage(
+                    role="user", 
+                    content=[
+                        ImageContent(type="input_text", text=prompt),
+                        *[ImageContent(type="input_image", image_url=image_url) for image_url in images]
+                    ]
+                ) if images else Message(role="user", content=prompt)
             ],
             text_format=format,
         )
@@ -35,4 +42,20 @@ class OpenAIClient(LLMClient):
 
     #TODO
     def generate_text_from_images(self, prompt, images, history = None):
-        pass
+        response = self.openai.responses.create(
+            model=self.config.model_name,
+            input=[
+                Message(role="system", content=self.config.system_prompt),
+                *(history or []),
+
+                ImageMessage(
+                    role="user", 
+                    content=[
+                        ImageContent(type="input_text", text=prompt),
+                        *[ImageContent(type="input_image", image_url=image_url) for image_url in images]
+                    ]
+                ),
+            ]
+        )
+
+        return response.output_text
