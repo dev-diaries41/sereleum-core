13879c10b06c83b1e7e46d31456169b28760dae6
d41dev <d41dev@proton.me>
2026-07-01 21:45:14 +0100

    update: import types from smartscan

--- sereleum/schemas/items/item.py
+++ sereleum/schemas/items/item.py
@@ -1,14 +1,10 @@
 
-from typing import Dict, TypeVar, Any, Generic
+from typing import Generic
+from smartscan.types import TData, TItem, TMetadata
 from pydantic import BaseModel
 
-TItem = TypeVar("TItem", bound=Any)
-TData = TypeVar("TData", bound=Any)
-TMetadata = TypeVar("TMetadata", bound=Dict)
 
 class Item(BaseModel, Generic[TData, TMetadata]):
     id: str
     data: TData
     metadata: TMetadata
-
-
