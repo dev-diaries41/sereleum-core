b944374184dc6e8b6d34c79d6111f00e55979f6b
d41dev <d41dev@proton.me>
2026-07-05 23:19:07 +0100

    update: use async redis client

--- api/redis.py
+++ api/redis.py
@@ -1,15 +1,17 @@
 import redis
+import redis.asyncio
 
 from dramatiq.brokers.redis import RedisBroker
 
 from sereleum.constants.db import REDIS_PASSWORD, REDIS_PORT, REDIS_HOST
 
-redis_client = redis.Redis(
+
+redis_client = redis.asyncio.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
     password=REDIS_PASSWORD,
     decode_responses=True,
-    db=2
+    db=2,
 )
 
 def get_broker():
